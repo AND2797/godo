21aef100dd63c0e1fe2e29eccaa0299d71a57cbf
aditya <aditya.das2797@gmail.com>
2021-03-21 18:36:22 -0400

    loading & reading init

--- fileops.go
+++ fileops.go
@@ -1,24 +1,53 @@
 package main
 
 
+import (
+        "fmt"
+        "encoding/json"
+        "os"
+    )
+
+type Task struct{
+    TaskID          int     `json:"taskID"`
+    TaskDescription string  `json:"taskDescription"`
+    Project         string  `json:"project"`
+}
+
+
+type Todo struct{
+    CurrentID     int      `json:"currentID"`
+    Tasks         []*Task  `json:"Task"`
+}
+
 func checkInit() bool {
 
     return false
 }
 
-func loadInit() {
+func loadInit()  {
+    initFile, _ := os.ReadFile("test.json")
+    var todo Todo
+    json.Unmarshal(initFile, &todo)
+    fmt.Println(todo)
+
+
+}
+
+func makeInit() {
+
+    data := Todo{Tasks: make([]*Task,0)}
+    file, _ := json.MarshalIndent(data, "", "")
+
+    _ = os.WriteFile("test.json", file, 0644)
 
 }
 
 func render() {
+
 }
-
-
-func makeInit() {
-}
-
 
 func addEntry() {
+
 }
 
 
--- main.go
+++ main.go
@@ -9,25 +9,28 @@ import (
 
 func main() {
 
-    argsWithoutProg := os.Args[1:]
-    fmt.Println(argsWithoutProg)
-
-    if !checkInit() {
-
-    } else {
-        loadInit()
-    }
-
-    if (argsWithoutProg[0] == "init") {
-        makeInit()
-    } else if (argsWithoutProg[0] == "a") {
-        addEntry()
-    } else if (argsWithoutProg[0] == "l") {
-        showEntries()
-    } else if (argsWithoutProg[0] == "c") {
-        checkEntry()
-    } else if (argsWithoutProg[0] == "d") {
-        deleteEntry()
-    }
-
+    makeInit()
+    loadInit()
+    fmt.Println("ok.", os.Args)
+//    argsWithoutProg := os.Args[1:]
+//    fmt.Println(argsWithoutProg)
+//
+//    if !checkInit() {
+//
+//    } else {
+//        loadInit()
+//    }
+//
+//    if (argsWithoutProg[0] == "init") {
+//        makeInit()
+//    } else if (argsWithoutProg[0] == "a") {
+//        addEntry()
+//    } else if (argsWithoutProg[0] == "l") {
+//        showEntries()
+//    } else if (argsWithoutProg[0] == "c") {
+//        checkEntry()
+//    } else if (argsWithoutProg[0] == "d") {
+//        deleteEntry()
+//    }
+//
 }

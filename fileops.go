8885e0dbe78749a888cf9a0f7648537fd1a58c74
aditya <aditya.das2797@gmail.com>
2021-03-22 01:24:05 -0400

    render list working

--- fileops.go
+++ fileops.go
@@ -53,26 +53,36 @@ func (todoList *Todo) saveInit() {
 }
 
 
-func render() {
+func render(task *Task) {
+
+    fmt.Printf("%d     %s\n", task.TaskID, task.TaskDescription)
 
 }
 
-func (todoList *Todo) addEntry() {
-
+func (todoList *Todo) addEntry(args []string) {
+    description := args[0]
     newTask := &Task{}
+    todoList.CurrentID = todoList.CurrentID + 1
     newTask.TaskID = todoList.CurrentID + 1
-    newTask.TaskDescription = "test"
+    newTask.TaskDescription = description
     todoList.Tasks = append(todoList.Tasks, newTask)
+    todoList.saveInit()
 
 }
 
 
-func showEntries() {
+func (todoList *Todo) showEntries() {
+
+    for _, val := range todoList.Tasks {
+        render(val)
+    }
+
+
 }
 
-func checkEntry() {
+func checkEntry(args []string) {
 }
 
-func deleteEntry() {
+func deleteEntry(args []string) {
 }
 
--- main.go
+++ main.go
@@ -2,37 +2,32 @@ package main
 
 import (
         "fmt"
+        "log"
         "os"
     )
 
 
 func main() {
 
-    makeInit()
-    todoList := loadInit()
-    todoList.addEntry()
-    todoList.saveInit()
-    todoList = loadInit()
-    fmt.Println("loaded",todoList.Tasks[0].TaskID, todoList.Tasks[0].TaskDescription)
-    argsWithoutProg := os.Args[1:]
-    fmt.Println(argsWithoutProg)
+    args := os.Args[1:]
+    fmt.Println(args)
 
-//    if !checkInit() {
-//
-//    } else {
-//        todoList := loadInit()
-//    }
-//
-//    if (argsWithoutProg[0] == "init") {
-//        makeInit()
-//    } else if (argsWithoutProg[0] == "a") {
-//        todoList.addEntry()
-//    } else if (argsWithoutProg[0] == "l") {
-//        showEntries()
-//    } else if (argsWithoutProg[0] == "c") {
-//        checkEntry()
-//    } else if (argsWithoutProg[0] == "d") {
-//        deleteEntry()
-//    }
+    if _, err := os.Stat("./test.json"); os.IsNotExist(err) {
+        fmt.Println("file does not exist")
+        log.Fatal("end")
+    }
+    todoList := loadInit()
+
+    if (args[0] == "init") {
+        makeInit()
+    } else if (args[0] == "a") {
+        todoList.addEntry(args[1:])
+    } else if (args[0] == "l") {
+        todoList.showEntries()
+    } else if (args[0] == "c") {
+        checkEntry(args[1:])
+    } else if (args[0] == "d") {
+        deleteEntry(args[1:])
+    }
 
 }

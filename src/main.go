cdc168b7860071333d0bd0d7787419e607f21b55
aditya <aditya.das2797@gmail.com>
2021-03-29 12:22:39 -0400

    changed elseif to switch

--- src/main.go
+++ src/main.go
@@ -22,6 +22,22 @@ func main() {
         return
     }
 
+    switch command := args[0]; command {
+        case "init":
+            makeInit()
+        case "a":
+            todoList.addEntry(args[1:])
+        case "l":
+            todoList.showEntries(args[1:])
+        case "c":
+            todoList.checkEntry(args[1:])
+        case "d":
+            todoList.deleteEntry(args[1:])
+        case "u":
+            todoList.uncheckEntry(args[1:])
+        }
+
+    /*
     if (args[0] == "init") {
         makeInit()
     } else if (args[0] == "a") {
@@ -34,6 +50,6 @@ func main() {
         todoList.deleteEntry(args[1:])
     } else if (args[0] == "u") {
         todoList.uncheckEntry(args[1:])
-    }
+    }*/
 
 }
